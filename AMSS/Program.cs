e36a677b20d1976454bd2cc0ae07a72b848d75a2
Phuoc Phan <114343125+PhuocPhann162@users.noreply.github.com>
2025-03-22 14:31:37 +0700

    integrate mailkit and setup smtp service, cron job to send email (#3)

--- AMSS/Program.cs
+++ AMSS/Program.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using Serilog;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -97,36 +96,6 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-    {
-        Description =
-             "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-             "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-             "Example: \"Bearer 12345abcdef\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Scheme = JwtBearerDefaults.AuthenticationScheme
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header,
-            },
-            new List<string>()
-        }
-    });
-});
 
 builder.Services.AddAuthorization();
 
---
+++ AMSS/Services/BackgroundJob/SendEmailJob.cs
@@ -0,0 +1,37 @@
+﻿using AMSS.Dto.Requests.Mails;
+using AMSS.Infrastructures.Interfaces;
+using AMSS.Services.IService.BackgroundJob;
+using MailKit.Net.Smtp;
+using System.Data;
+
+namespace AMSS.Services.BackgroundJob
+{
+    public class SendEmailJob(
+        ILogger _logger,
+        ISmtpService _smtpService
+        ) : ISendEmailJob
+    {
+        public async Task Invoke(MailRequest request)
+        {
+            _logger.LogInformation("Starting send email process...");
+
+            try
+            {
+                // Send Email using SendGrid
+                var response = await _smtpService.SendEmailAsync(request);
+
+                if (response.StatusCode is SmtpStatusCode.Ok)
+                {
+                    _logger.LogInformation("Email sent successfully.");
+                }
+
+                _logger.LogWarning("Queue end ====> {DatTime}", DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss"));
+            }
+            catch (DBConcurrencyException ex)
+            {
+                _logger.LogError(ex, "Error occurred while sending email.");
+                throw; // Re-throw to ensure Hangfire can retry
+            }
+        }
+    }
+}
